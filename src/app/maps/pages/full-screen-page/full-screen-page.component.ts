ff134f6ab15657cb1ce027e3a4382398d43a7d2c
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-28 17:41:05 -0300

    Zoom range

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { Map } from 'maplibre-gl';
+
 
 @Component({
   standalone: false,
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
+  ngAfterViewInit(): void {
+    
+      if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+      const map = new Map({
+        container: this.divMap.nativeElement, // container id
+        style:
+          'https://api.maptiler.com/maps/basic/style.json?key=dPx3ZgsnlO7XVPp3E8s3', //'https://demotiles.maplibre.org/style.json', // stylesheet location
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      console.log('Mapa inicializado:', map);
+    
+  }
 }
--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from 'maplibre-gl';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   standalone: false,
   templateUrl: './zoom-page.component.html',
   styleUrl: './zoom-page.component.css'
 })
-export class ZoomPageComponent {
+export class ZoomPageComponent implements AfterViewInit, OnDestroy{
+ 
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentCenter: LngLat = new LngLat(-63.17603657179461, 9.748762730497646);
+
+  ngAfterViewInit(): void {
+   
+      if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+      this.map = new Map({
+        container: this.divMap.nativeElement, // container id
+        style:
+          `https://api.maptiler.com/maps/basic/style.json?key=${environment.mapbox_key}`, //'https://demotiles.maplibre.org/style.json', // stylesheet location
+        center: this.currentCenter, // starting position [lng, lat]
+        zoom: this.zoom, // starting zoom
+      });
+      console.log('Mapa inicializado:', map);
+   this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove(); //Remuevo todo el mapa
+  }
+
+  mapListeners(): void {
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', () => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', () => {
+      if (this.map!.getZoom() > 18) {
+        this.map!.zoomOut();
+      }
+      if (this.map!.getZoom() < 0) {
+        this.map!.zoomIn();
+      }
+    });
+
+    this.map.on('move', () => {
+      this.currentCenter = this.map!.getCenter();
+    });
+  }
+
+  zoomIn(): void {
+    this.map?.zoomIn();
+  }
+
+  zoomOut(): void {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    //this.map?.setZoom(this.zoom); // Esta es otra forma
+    this.map?.zoomTo(this.zoom);
+  }
 }
