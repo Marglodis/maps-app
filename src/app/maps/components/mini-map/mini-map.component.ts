8bd00f91e74e249ef4e0c4a3f21d1985647bd70b
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-28 08:29:42 -0300

    Estructura y rutas del proyecto

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  standalone: false,
+  templateUrl: './mini-map.component.html',
+  styleUrl: './mini-map.component.css'
+})
+export class MiniMapComponent {
+
+}
---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  templateUrl: './markers-page.component.html',
+  styleUrl: './markers-page.component.css'
+})
+export class MarkersPageComponent {
+
+}
